b9228e9c7bc60d341ca264340cffad1f0e264b49
Marilynn Stone <hello@marilynnstone.com>
2022-11-21 11:55:51 -0800

    Update to use React Router.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./components/App/App";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Router>
     <App />
-  </BrowserRouter>,
+  </Router>,
   document.getElementById("root")
 );
