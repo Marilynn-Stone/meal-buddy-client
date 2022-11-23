e021f9cbfa238a79297538b104a1290f21290399
Marilynn Stone <hello@marilynnstone.com>
2022-11-23 15:02:57 -0800

    Update to show side bar and home page.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Header from "../Header";
 // import About from "../About";
-import Home from "../Home";
+// import Home from "../Home";
 import Login from "../Login/Login";
 import useToken from "./useToken";
 
@@ -14,19 +14,36 @@ function App() {
     return <Login setToken={setToken} />;
   }
 
+  const App = () => (
+    <div>
+      <Routes>
+        {/* <Route path="/list" component={GroceryList} /> */}
+        <Route path="header" element={<Header />} />
+        <Route path="login" element={<Login />} />
+        <Route exact path="/" component={App} />
+      </Routes>
+    </div>
+  );
+
   return (
-    <div className="wrapper">
-      <h1>Meal-Buddy</h1>
-      <div className="App">
-        <div>
-          <Routes>
-            <Route path="home" element={<Home />} />
-            <Route path="header" element={<Header />} />
-            <Route path="login" element={<Login />} />
-          </Routes>
+    <>
+      <Header />
+      <div className="wrapper">
+        <h1>Meal-Buddy</h1>
+        <div className="Home">
+          <div className="sidebar">
+            <p className="sidebar-text">links to:</p>
+            <li className="sidebar-text">About</li>
+            <li className="sidebar-text">How To</li>
+            <li className="sidebar-text">Menu</li>
+          </div>
+          <div className="mainpage">
+            <App />
+            <h1>Home Page "/"</h1>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
