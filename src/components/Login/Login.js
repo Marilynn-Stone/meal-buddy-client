0cb12bbe165414e41d837c8daec61ae806f6e535
Marilynn Stone <hello@marilynnstone.com>
2022-11-23 14:31:13 -0800

    Add fetch and handleSubmit functionality.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,17 +1,59 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
 import "./Login.scss";
 
-export default function Login() {
+async function loginUser(credentials) {
+  // check to see what is being returned
+  fetch("http://localhost:8080/api/users/login")
+    .then((res) => res.text()) // convert to plain text
+    .then((text) => console.log(text)); // then log it out
+
+  return fetch("http://localhost:8080/api/users/login", {
+    method: "POST",
+    // mode: "cors",
+    // credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Success:", data);
+    })
+    .catch((error) => {
+      console.error("There has been a problem with your fetch operation:", {
+        error,
+      });
+    });
+}
+export default function Login({ setToken }) {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const token = await loginUser({
+      email,
+      password,
+    });
+    setToken(token);
+  };
+
   return (
     <div className="login-wrapper">
       <h1>Please Log In</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
-          <p>Username</p>
-          <input type="text" />
+          <p>Email</p>
+          <input type="text" onChange={(e) => setEmail(e.target.value)} />
         </label>
         <label>
           <p>Password</p>
-          <input type="password" />
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </label>
         <div>
           <button type="submit">Submit</button>
@@ -20,3 +62,7 @@ export default function Login() {
     </div>
   );
 }
+
+Login.propTypes = {
+  setToken: PropTypes.func.isRequired,
+};
