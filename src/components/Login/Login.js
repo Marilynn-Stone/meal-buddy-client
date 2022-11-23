f499e64b0996e73b3ffd383288d966de23cca103
Paul Taylor <paul@whammydiver.com>
2022-11-22 19:26:35 -0500

    express server called with login and res received

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,26 +1,51 @@
 import "./Login.scss";
-import "src/hooks/serverAndDBCalls.js"
-import { get } from "request";
+import axios from 'axios';
+import React, { useState } from 'react';
 
-function onClick(email, password) {
-  fetch()
-}
+// import loginCall from "../../hooks/serverAndDBCalls"
+
 
 export default function Login() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  
+  function loginCall(email, password) {
+    axios.post("http://localhost:3001/login", { email, password })
+      .then((response)=> {
+        console.log(response);
+        }).catch(error => console.log(error))
+  }
+  
+  function onClick(email, password) {
+    loginCall(email, password)
+  }
+  
+
   return (
     <div className="login-wrapper">
       <h1>Please Log In</h1>
-      <form>
+      <form autoComplete="off" onSubmit={event => event.preventDefault()}>
         <label>
           <p>Email</p>
-          <input type="text" name="email" value={email}/>
+          <input 
+            type="text" 
+            name="email" 
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
         </label>
         <label>
           <p>Password</p>
-          <input type="password" name="password" value={password}/>
+          <input 
+            type="password" 
+            name="password" 
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
         </label>
         <div>
-          <button onClick={onClick(email, password)} type="submit">Submit</button>
+          <button onClick={ () => onClick(email, password)} type="submit">Submit</button>
         </div>
       </form>
     </div>
--- src/hooks/serverAndDBCalls.js
+++ src/hooks/serverAndDBCalls.js
@@ -1,15 +1,9 @@
-require axios from "Axios";
+// import axios from 'axios';
 
+// const loginCall = function(email, password) {
+//   axios.post("http://localhost:3001/login", { email, password })
+//     .then((response)=> {
+//       console.log(response);
+//       }).catch(error => console.log(error))
+// }
 
-export default function serverAndDBCalls() {
-
-  function loginCall(email, password) {
-    axios.post("http://localhost:3001/login", { email, password })
-      .then((response)=> {
-        console.log(response);
-        }).catch(error => console.log(error))
-  }
-
-  return { loginCall }
-
-}
