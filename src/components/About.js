902be5c128bbf4ce1b51963a7184014eca61b0e9
Marilynn Stone <hello@marilynnstone.com>
2022-11-23 13:38:14 -0800

    Minor semantic modifications.

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,5 @@
 export default function About() {
-  render(
-    <div>
-      This MF app rules the skool!
-
-      Let us tell you all about it...
-    </div>
+  return (
+    <div>This MF app rules the skool! Let us tell you all about it...</div>
   );
 }
