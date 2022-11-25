100ce86c0f54908600d4e183e48a0d1469dc352f
trevorfunk <trevorjonfunk@gmail.com>
2022-11-24 20:32:55 -0800

    create basic signup page and modal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Route, Routes, Link } from "react-router-dom";
 
-import './App.scss';
-import Header from './components/Header';
+import "./App.scss";
+import Header from "./components/Header";
 import Login from "./components/Login/Login";
-import MainPage from './components/MainPage';
-
+import MainPage from "./components/MainPage";
+import Modal from "./components/Modal";
+import SignUpPage from "./components/SignUpPage";
 
 //import GroceryList from './components/GroceryList/GroceryList';
 
@@ -18,23 +19,24 @@ function App() {
   // }
 
   // const App = () => (
-    <div>
-      <Routes>
-        {/* <Route path="/list" component={GroceryList} /> */}
-        <Route path="header" element={<Header />} />
-        <Route path="login" element={<Login />} />
-        <Route exact path="/" component={App} />
-      </Routes>
-    </div>
-  );
+  <div>
+    <Routes>
+      {/* <Route path="/list" component={GroceryList} /> */}
+      <Route path="header" element={<Header />} />
+      <Route path="login" element={<Login />} />
+      <Route exact path="/" component={App} />
+    </Routes>
+  </div>;
 
   return (
     <>
       <Header />
       <MainPage />
-      <Button />
+      {/* <Button /> */}
       <Login />
-    
+      {/* <Modal>
+        <SignUpPage />
+      </Modal> */}
     </>
   );
 }
