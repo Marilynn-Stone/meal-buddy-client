7250c282a81ab72be47e7f6e9a942379239c4f08
Marilynn Stone <hello@marilynnstone.com>
2022-11-25 10:17:24 -0800

    Add header code from original repo.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,18 @@
 import { Link } from "react-router-dom";
-import React from 'react'; //optional
+import React from "react"; //optional
 
-export default function Header(){
+export default function Header() {
   return (
     <>
-    <div className="header">
-          
-            <h1 className="child bounce">MealBuddy</h1>
-            
-         </div>
-      
-      </>
-
+      <div className="header">
+        <h1 className="child bounce">MealBuddy</h1>
+        <h3>Login</h3>
+        <h3>Logout</h3>
+        <h3>Sign up!</h3>
+        <h3>Profile</h3>
+        <h3>Account Settings</h3>
+        <h3>Dietary Settings</h3>
+      </div>
+    </>
   );
 }
-
-// ROUTES
-
-// - / (get)
-// - /login (get, post)
-// - / logout (post)
-// - /sign up (get, create, delete)
-// - /profile (get, post, update)
-//      - /account info
-//      -/dietary settings
