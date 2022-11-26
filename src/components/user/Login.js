a3faa501c84bff8b16a59fe772fc185478042795
Marilynn Stone <hello@marilynnstone.com>
2022-11-26 06:28:27 -0800

    Remove fetch() and add axios hook.

--- src/components/user/Login.js
+++ src/components/user/Login.js
@@ -1,60 +1,36 @@
 import { useState } from "react";
-import { useCookies } from "react-cookie";
+// import { useCookies } from "react-cookie";
 // import PropTypes from "prop-types";
 import "./Login.scss";
 
 import loginCall from "../../hooks/serverAndDBCalls";
 
-async function loginUser(credentials) {
-  // check to see what is being returned
-  fetch("http://localhost:8080/api/users/login")
-    .then((res) => res.text()) // convert to plain text
-    .then((text) => console.log(text)); // then log it out
-
-  return fetch("http://localhost:8080/api/users/login", {
-    method: "POST",
-    // mode: "cors",
-    // credentials: "include",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(credentials),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("Success:", data);
-    })
-    .catch((error) => {
-      console.error("There has been a problem with your fetch operation:", {
-        error,
-      });
-    });
-}
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  // function onClick(email, password) {
-  //   loginCall(email, password);
-  // }
-
-  const [cookies, setCookie] = useCookies(["user"]);
-
-  const handleSubmit = async (e) => {
+  function handleSubmit(e) {
     e.preventDefault();
-    const userData = await loginUser({
-      email,
-      password,
-    });
-    setCookie("user", userData, {
-      path: "/",
-    });
-  };
+    loginCall(email, password);
+  }
+
+  // const [cookies, setCookie] = useCookies(["user"]);
+
+  // const handleSubmit = new Promise(function(resolve[,reject])) {
+  //   e.preventDefault();
+  //   const userData = await console.log("userData:", userData);
+  //   loginCall({
+  //     userData,
+  //   });
+  // setCookie("user", userData, {
+  //   path: "/",
+  // });
+  // };
 
   return (
     <div className="login-wrapper">
       <h1>Please Log In</h1>
-      {cookies.user && <p>{cookies.user}</p>}
+      {/* {cookies.user && <p>{cookies.user}</p>} */}
       <form autoComplete="off" onSubmit={handleSubmit}>
         <label>
           <p>Email</p>
