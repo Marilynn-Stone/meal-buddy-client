4def0bc40cfb9a7c02a2b1b66791e42b8de1fa0b
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-11-30 17:24:04 -0400

    fix home page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,55 +1,28 @@
-//import { Link } from "react-router-dom";
-
+import { Link } from "react-router-dom";
 import "./App.scss";
-//import Header from "../Header";
-//import Sidebar from "../Sidebar";
+
 import MainPage from "../MainPage";
-// import Modal from "../Modal";
-// import SignUp from "../SignUpPage";
-// import GroceryList from "../GroceryList/GroceryList";
-// import DailyMenu from "../Menu/DailyMenu";
-// import FullPageMenuItem from "../Menu/FullPageMenuItem";
-// import Menu from "../Menu/Menu";
-// import MiniMenuItem from "../Menu/MiniMenuItem";
-// import AccountInfo from "../user/AccountInfo";
-// import Dashboard from "../user/Dashboard";
-// import DietarySettings from "../user/DietarySettings";
-// import Login from "../user/Login";
-// import UserIndex from "../user/UserIndex";
-// import About from "../About";
-// import HowTo from "../HowTo";
-// import LogOut from "../LogOut";
-// import PopUp from "../PopUp";
-// import RecipeItem from "../Menu/RecipeItem";
-// import WelcomeBack from "../WelcomeBack";
+
 
 function App(session) {
 
   return (
     <>
-      <Header />
-      <Sidebar  />
-      <div className="wrapper">
-        <div className="Home">
-          <div className="sidebar">
-            <p className="sidebar-text">links to:</p>
-            <li className="sidebar-text">
-              <Link to={"/about"}>About</Link>
-            </li>
-            <li className="sidebar-text">
-              <Link to={"/howTo"}>How To</Link>
-            </li>
-            <li className="sidebar-text">
-              <Link to={"/menu"}>Menu</Link>
-            </li>
-          </div>
-          <div className="mainpage">
-            <MainPage />
-          </div>
-        </div>
-      </div>
+      <MainPage />
+      {/* <GroceryList />
+      <Menu />
+      <RecipeItem />
+      <FullPageMenuItem />
+      <Dashboard />
+      <Login />
+      <UserIndex />
+      <About />
+      <HowTo />
+      <WelcomeBack />
+      <LogOut />
+      <PopUp />
+      <SignUp /> */}
     </>
   );
 }
-
 export default App;
