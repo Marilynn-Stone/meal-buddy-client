e4263332a061464a0436f47ab6fe4c4ef57d0e0a
Marilynn Stone <hello@marilynnstone.com>
2022-11-30 13:00:40 -0800

    Remove Mainpage link.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,27 +3,9 @@ import { Link } from "react-router-dom";
 import "./App.scss";
 import Header from "../Header";
 import Sidebar from "../Sidebar";
-import MainPage from "../MainPage";
-// import Modal from "../Modal";
-// import SignUp from "../SignUpPage";
-// import GroceryList from "../GroceryList/GroceryList";
-// import DailyMenu from "../Menu/DailyMenu";
-// import FullPageMenuItem from "../Menu/FullPageMenuItem";
-// import Menu from "../Menu/Menu";
-// import MiniMenuItem from "../Menu/MiniMenuItem";
-// import AccountInfo from "../user/AccountInfo";
-// import Dashboard from "../user/Dashboard";
-// import DietarySettings from "../user/DietarySettings";
-// import Login from "../user/Login";
-// import UserIndex from "../user/UserIndex";
-// import About from "../About";
-// import HowTo from "../HowTo";
-// import LogOut from "../LogOut";
-// import PopUp from "../PopUp";
-// import RecipeItem from "../Menu/RecipeItem";
-// import WelcomeBack from "../WelcomeBack";
+// import MainPage from "../MainPage";
 
-function App(session) {
+function App() {
 
   return (
     <>
@@ -44,7 +26,7 @@ function App(session) {
             </li>
           </div>
           <div className="mainpage">
-            <MainPage />
+            {/* <MainPage /> */}
           </div>
         </div>
       </div>
