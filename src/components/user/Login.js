05c70bd35230ebf91f440bdf4b24c7dda93307dd
Marilynn Stone <hello@marilynnstone.com>
2022-11-30 14:31:43 -0800

    Create login functionality.

--- src/components/user/Login.js
+++ src/components/user/Login.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "../../styles/login.scss";
-import loginCall from "../../hooks/serverAndDBCalls";
+import { loginCall } from "../../hooks/serverAndDBCalls";
 import { useNavigate } from "react-router-dom";
 import Header from "../Header";
 import Sidebar from "../Sidebar";
