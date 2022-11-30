6bd8324cf822a2ebea1ce7dcb72f7a5b69fef5f2
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-11-30 16:18:40 -0400

    i dont know why i am making so many useless commits right now git is mad at me

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,25 +5,25 @@ import "./App.scss";
 //import Sidebar from "../Sidebar";
 import MainPage from "../MainPage";
 //import Modal from "../Modal";
-import SignUp from "../SignUpPage";
-import Modal from "../Modal";
-import SignUp from "../SignUp";
-import GroceryList from "../GroceryList/GroceryList";
-//import DailyMenu from "../Menu/DailyMenu";
-import FullPageMenuItem from "../Menu/FullPageMenuItem";
-import Menu from "../Menu/Menu";
-//import MiniMenuItem from "../Menu/MiniMenuItem";
-//import AccountInfo from "../user/AccountInfo";
-import Dashboard from "../user/Dashboard";
-//import DietarySettings from "../user/DietarySettings";
-import Login from "../user/Login";
-import UserIndex from "../user/UserIndex";
-import About from "../About";
-import HowTo from "../HowTo";
-import LogOut from "../LogOut";
-import PopUp from "../PopUp";
-import RecipeItem from "../Menu/RecipeItem";
-import WelcomeBack from "../WelcomeBack";
+// import SignUp from "../SignUpPage";
+// import Modal from "../Modal";
+// //import SignUp from "../SignUp";
+// import GroceryList from "../GroceryList/GroceryList";
+// //import DailyMenu from "../Menu/DailyMenu";
+// import FullPageMenuItem from "../Menu/FullPageMenuItem";
+// import Menu from "../Menu/Menu";
+// //import MiniMenuItem from "../Menu/MiniMenuItem";
+// //import AccountInfo from "../user/AccountInfo";
+// import Dashboard from "../user/Dashboard";
+// //import DietarySettings from "../user/DietarySettings";
+// import Login from "../user/Login";
+// import UserIndex from "../user/UserIndex";
+// import About from "../About";
+// import HowTo from "../HowTo";
+// import LogOut from "../LogOut";
+// import PopUp from "../PopUp";
+// import RecipeItem from "../Menu/RecipeItem";
+// import WelcomeBack from "../WelcomeBack";
 
 function App(session) {
 
@@ -31,10 +31,8 @@ function App(session) {
     <>
 
       
-      <MainPage checkCookie={checkCookie} />
-      {/* <GroceryList /> */}
-
       <MainPage />
+      
       {/* <GroceryList />
       <Menu />
       <RecipeItem />
