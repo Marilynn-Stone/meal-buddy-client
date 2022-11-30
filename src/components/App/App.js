ffa05ae6b51962029bda0065af7ddb347f33c2ed
Marilynn Stone <hello@marilynnstone.com>
2022-11-30 13:24:03 -0800

    Reorganize home page appearance.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,37 +1,15 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 import "./App.scss";
 import Header from "../Header";
 import Sidebar from "../Sidebar";
-// import MainPage from "../MainPage";
 
-function App() {
+export default function App() {
 
   return (
     <>
       <Header />
-      <Sidebar  />
-      {/* <div className="wrapper">
-        <div className="Home">
-          <div className="sidebar">
-            <p className="sidebar-text">links to:</p>
-            <li className="sidebar-text">
-              <Link to={"/about"}>About</Link>
-            </li>
-            <li className="sidebar-text">
-              <Link to={"/howTo"}>How To</Link>
-            </li>
-            <li className="sidebar-text">
-              <Link to={"/menu"}>Menu</Link>
-            </li>
-          </div>
-          {/* <div className="mainpage">
-            <MainPage />
-          </div> */}
-        {/* </div> */}
-      {/* </div> */}
+      <Sidebar />
     </>
   );
 }
-
-export default App;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,30 +1,23 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
-
-
 export default function Sidebar() {
 
   return (
-
   <div className="sidebar">
     <li className="sidebar-text">
-      <Link to={"/menu"}>Menu</Link>
+      <Link to={"menu"}>Menu</Link>
     </li>
     <li className="sidebar-text">
-      <Link to={"list"}>Grocery List</Link>
+      <Link to={"groceryList"}>Grocery List</Link>
     </li>
     <li className="sidebar-text">
-      <Link to={"dashboard"}>Settings</Link>
+      <Link to={"about"}>About</Link>
     </li>
     <li className="sidebar-text">
-      <Link to={"/about"}>About</Link>
-    </li>
-    <li className="sidebar-text">
-      <Link to={"/howTo"}>How To</Link>
-    </li>
-    
+      <Link to={"howTo"}>How To</Link>
+    </li>  
   </div>
-
   );
+
 }
