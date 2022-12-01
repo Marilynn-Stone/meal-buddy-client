3dd4d7d77618dcc6bb2a2745429d5a78800cb425
Paul Taylor <paul@whammydiver.com>
2022-11-30 22:39:59 -0400

    pseudo first commit

--- src/hooks/serverAndDBCalls.js
+++ src/hooks/serverAndDBCalls.js
@@ -2,14 +2,14 @@ import axios from "axios";
 
 const loginCall = (email, password) => {
 
-    axios.post("http://localhost:8080/users/login", { email, password }, {withCredentials: true})
+    axios.post("http://localhost:8080/users/login", { email, password, withCredentials: true })
     .then((res) => {
       console.log("res.data:", res.data);
-      localStorage.setItem("userID", res.data.user);
+      localStorage.setItem("userID", res.data);
     })
     .catch((err) => {
       console.log("error with login", err.res.data);
     });
-  };
+};
 
-  export { loginCall };
+export { loginCall };
