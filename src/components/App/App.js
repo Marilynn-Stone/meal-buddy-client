4b456cf5b8a57aed7fe2334f9da9a0ffc8af3307
Marilynn Stone <hello@marilynnstone.com>
2022-12-03 12:32:00 -0800

    Remove App.js.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,18 @@
-import "./App.scss";
+// import "./App.scss";
 
 // import MainPage from "../MainPage";
 // import Menu from "../Menu/Menu";
-import ButtonExperiment from "../ButtonExperiment";
-import RecipeItem from "../Menu/RecipeItem";
-
-export default function App() {
-  return (
-    <>
+// import ButtonExperiment from "../ButtonExperiment";
+// import RecipeItem from "../Menu/RecipeItem";
+// 
+// export default function App() {
+  // return (
+    // <>
      
       {/* <ButtonExperiment /> */}
       {/* <RecipeItem /> */}
-    </>
-  );
+    // </>
+  // );
   // return (
     // <>
      
@@ -21,4 +21,4 @@ export default function App() {
         
     // </>
   // );
-}
+// }
