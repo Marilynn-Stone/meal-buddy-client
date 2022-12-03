58a1ae7e914b26860f549c827efe2a84e982ab53
Marilynn Stone <hello@marilynnstone.com>
2022-12-03 12:32:39 -0800

    Update router routes.

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import "./index.scss";
 import App from "./components/App/App";
-import Modal from "./components/Modal";
-import PopUp from "./components/PopUp";
 import WelcomeBack from "./components/WelcomeBack";
 import About from "./components/About";
 import Menu from "./components/Menu/Menu";
 import HowTo from "./components/HowTo";
 import SignUp from "./components/SignUp";
 import Login from "./components/user/Login";
-import UserIndex from "./components/user/UserIndex";
 import AccountInfo from "./components/user/AccountInfo";
 import Dashboard from "./components/user/Dashboard";
 import DailyMenu from "./components/Menu/DailyMenu";
@@ -21,6 +18,7 @@ import DietarySettings from "./components/user/DietarySettings";
 import GroceryList from "./components/GroceryList/GroceryList";
 import LogOut from "./components/LogOut";
 import Header from "./components/Header";
+import MiniMenuItem from "./components/Menu/MiniMenuItem";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
@@ -29,23 +27,26 @@ root.render(
   <Router>
     <Routes>
       <Route path="/" element={<Header />} >
-          <Route path="/" element={<App />} />
-          <Route path="logIn" element={<Login />} />
-          <Route path="logOut" element={<LogOut />} />
-          <Route path="signUp" element={<SignUp />} />
-          <Route path="menu" element={<Menu />} />
-          <Route path="profile" element={<UserIndex />} />
-          <Route path="groceryList" element={<GroceryList />} />
-          <Route path="about" element={<About />} />
-          <Route path="welcomeBack" element={<WelcomeBack />} />
-          <Route path="accountInfo" element={<AccountInfo />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="dailyMenu" element={<DailyMenu />} />
-          <Route path="TableMenu" element={<TableMenu />} />
-          <Route path="RecipeItem" element={<RecipeItem />} />
-          <Route path="TableMenuItem" element={<TableMenuItem />} />
-          <Route path="dietarySettings" element={<DietarySettings />} />
-          <Route path="howTo" element={<HowTo />} />
+        <Route path="about" element={<About />} />
+        <Route path="howTo" element={<HowTo />} />
+        <Route path="logIn" element={<Login />} />
+        <Route path="signUp" element={<SignUp />} />
+        <Route path="logOut" element={<LogOut />} />
+
+        <Route path="menu" element={<Menu />} />
+        <Route path="dailyMenu" element={<DailyMenu />} />
+        <Route path="miniMenuItem" element={<MiniMenuItem />} />
+
+        <Route path="TableMenu" element={<TableMenu />} />
+        <Route path="TableMenuItem" element={<TableMenuItem />} />
+
+        <Route path="groceryList" element={<GroceryList />} />
+
+        <Route path="RecipeItem" element={<RecipeItem />} />
+
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="dietarySettings" element={<DietarySettings />} />
+        <Route path="accountInfo" element={<AccountInfo />} />
          
       </Route>
     </Routes>
