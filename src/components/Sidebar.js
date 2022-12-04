c2f4e1152706d590d96108371448fad4b7f9ff4a
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-12-03 20:36:40 -0400

    dashboard update

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,9 +9,7 @@ export default function Sidebar() {
     <li className="sidebar-text">
       <Link to={"/menu"}>Menu</Link>
     </li>
-    <li className="sidebar-text">
-      <Link to={"/groceryList"}>Grocery List</Link>
-    </li>
+    
     <li className="sidebar-text">
       <Link to={"/about"}>About</Link>
     </li>
