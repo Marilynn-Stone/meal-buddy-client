50280aa827e984945b88f2294b99e7f6b76d8f68
Paul Taylor <paul@whammydiver.com>
2022-12-04 10:39:01 -0500

    Revert "Main recipe page rendering"

--- src/components/Menu/TableMenuItem.js
+++ src/components/Menu/TableMenuItem.js
@@ -2,11 +2,10 @@ import { Link } from "react-router-dom"
 
 export default function TableMenuItem(props) {
   return (
-    <section className="TableMenuItem" item_id={props.item_id}>
+    <section className="TableMenuItem">
       <p>{props.day}</p>
       <p>{props.meal}</p>
-      <Link to={`/menu/meal/${props.item_id}`} >{props.name}</Link>
-      
+      <Link to={`/menu/meal/${props.item_id}`}>{props.name}</Link>
     </section>
   )
 }
