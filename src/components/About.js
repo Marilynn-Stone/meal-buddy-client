cf74b69ae49c12433af499d7a616dfb51302c2bd
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-12-05 16:01:44 -0400

    more about styling

--- src/components/About.js
+++ src/components/About.js
@@ -6,6 +6,9 @@ export default function About() {
    <section className="about-us">
     <h1>About Us</h1>
     <div>This MF app rules the skool! Let us tell you all about it...</div>
+    <p> More interesting things and stuff to be inserted here.</p>
+    <p> More interesting things and stuff to be inserted here.</p>
+    <p> More interesting things and stuff to be inserted here.</p>
   </section>
   </div>
   </>
