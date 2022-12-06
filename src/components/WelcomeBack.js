c304bd674d5ef080b50801fd5fa03c8c93472001
Marilynn Stone <hello@marilynnstone.com>
2022-12-06 15:43:19 -0800

    Create sign up Welcome To routing.

--- src/components/WelcomeBack.js
+++ src/components/WelcomeBack.js
@@ -1,4 +1,4 @@
-export default function SidebarLoggedIn() {
+export default function WelcomeBack() {
   return (
     <section className="welcome-back">
       <p>Welcome Back, Buddy!</p>
---
+++ src/components/WelcomeTo.js
@@ -0,0 +1,7 @@
+export default function WelcomeTo() {
+  return (
+    <section className="welcome">
+      <p>Welcome, Buddy!</p>
+    </section>
+  );
+}
