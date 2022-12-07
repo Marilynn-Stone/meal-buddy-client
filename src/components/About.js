ee0304ba112b01ea757f7cd9ddf06ba9ffad3440
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-12-06 20:56:27 -0400

    fix

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ export default function About() {
    <section className="about-us">
     
     <h1>MealBuddy takes the guess-work out of meal-planning!</h1>
-    <p> Simply tell us about your daily calorie goals, dietary category, and/or dietary restrictions, and we'll build you a customized meal-plan (including recipes!)! </p>
+    <p> Simply tell us about your daily calorie goals, dietary category, and/or dietary restrictions, and we'll build you a customized meal-plan (including recipes)! </p>
     <p> Still not sure about MealBuddy? </p>
     <p> Try out our Demo for a sample daily meal plan. </p>
   </section>
